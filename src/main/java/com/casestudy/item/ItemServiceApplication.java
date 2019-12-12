10bd0f8398f0324489065f06df73e01c2d5df9ea
sandisan <sandicha@in.ibm.com>
2019-12-12 21:26:45 +0530

    Update ItemServiceApplication.java

--- src/main/java/com/casestudy/item/ItemServiceApplication.java
+++ src/main/java/com/casestudy/item/ItemServiceApplication.java
@@ -2,11 +2,11 @@ package com.casestudy.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableJpaRepositories
 @SpringBootApplication
 public class ItemServiceApplication {
